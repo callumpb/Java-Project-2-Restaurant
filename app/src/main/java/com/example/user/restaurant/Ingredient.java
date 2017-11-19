d6e9fbcfc0b57ab5643c82e54e1dfa27ba39fd81
Callum <callumpb@hotmail.com>
2017-11-19 22:35:34 +0000

    created Ingredient class

--- app/src/main/java/com/example/user/restaurant/Ingredient.java
+++ app/src/main/java/com/example/user/restaurant/Ingredient.java
@@ -4,5 +4,11 @@ package com.example.user.restaurant;
  * Created by user on 19/11/2017.
  */
 
+//may change to a hash map?
 public class Ingredient {
+    private String name;
+
+    public Ingredient(String name) {
+        this.name = name;
+    }
 }
