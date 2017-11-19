34e39a53c2efd7d83d7554ef14d9978e63082ea8
Callum <callumpb@hotmail.com>
2017-11-19 21:49:50 +0000

    initial Table instantiation

--- app/src/main/java/com/example/user/restaurant/Table.java
+++ app/src/main/java/com/example/user/restaurant/Table.java
@@ -1,8 +1,17 @@
 package com.example.user.restaurant;
 
+import java.util.ArrayList;
+
 /**
  * Created by user on 19/11/2017.
  */
 
 public class Table {
+    private ArrayList<Patron> patrons;
+
+    public Table() {
+        this.patrons = new ArrayList<Patron>();
+    }
+
+    
 }
