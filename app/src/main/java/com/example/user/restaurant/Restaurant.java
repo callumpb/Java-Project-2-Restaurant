b51a12ef95402cffafcb6ace7e402ebed38ca01c
Callum <callumpb@hotmail.com>
2017-11-19 21:41:18 +0000

    initial Restaurant instantiation

--- app/src/main/java/com/example/user/restaurant/Restaurant.java
+++ app/src/main/java/com/example/user/restaurant/Restaurant.java
@@ -2,6 +2,7 @@ package com.example.user.restaurant;
 
 import com.sun.tools.javac.util.Name;
 
+import java.awt.Menu;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
@@ -13,7 +14,21 @@ public class Restaurant {
 
     private String name;
     private float budget;
-    private ArrayList<Table> tables;
-    
+
+    //change Name.Table
+    private ArrayList<Name.Table> tables;
+    Menu menu;
+
     //Menu menu;
+
+    public Restaurant(String name, float budget) {
+        this.name = name;
+        this.budget = budget;
+        //change <>
+        this.tables = new ArrayList<Name.Table>();
+        Menu menu = new Menu();
+    }
+
+
 }
+
