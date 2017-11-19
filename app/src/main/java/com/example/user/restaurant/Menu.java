393f20548073c15ffe067ed6fc750734f199a241
Callum <callumpb@hotmail.com>
2017-11-19 22:46:34 +0000

    initial Menu instantiation

--- app/src/main/java/com/example/user/restaurant/Menu.java
+++ app/src/main/java/com/example/user/restaurant/Menu.java
@@ -1,8 +1,15 @@
 package com.example.user.restaurant;
 
+import java.util.ArrayList;
+
 /**
  * Created by user on 19/11/2017.
  */
 
 public class Menu {
+    private ArrayList<Dish> dishes;
+
+    public Menu() {
+        this.dishes = new ArrayList<Dish>();
+    }
 }
