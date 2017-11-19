7e6d00ab918e4c3736ed99942325dfc46ad7ea49
Callum <callumpb@hotmail.com>
2017-11-19 22:10:57 +0000

    initial Dish instantiation

--- app/src/main/java/com/example/user/restaurant/Dish.java
+++ app/src/main/java/com/example/user/restaurant/Dish.java
@@ -1,8 +1,19 @@
 package com.example.user.restaurant;
 
+import java.util.ArrayList;
+
 /**
  * Created by user on 19/11/2017.
  */
 
 public class Dish {
+    private String name;
+    private float price;
+    private ArrayList<Ingredient> ingredients;
+
+    public Dish(String name, float price) {
+        this.name = name;
+        this.price = price;
+        this.ingredients = new ArrayList<Ingredient>();
+    }
 }
