6a189a31d4c68cc98f8f9cc62b73650fb21dbb75
Callum <callumpb@hotmail.com>
2017-11-19 21:27:07 +0000

    initial commit

---
+++ app/src/main/java/com/example/user/restaurant/Restaurant.java
@@ -0,0 +1,19 @@
+package com.example.user.restaurant;
+
+import com.sun.tools.javac.util.Name;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * Created by user on 17/11/2017.
+ */
+
+public class Restaurant {
+
+    private String name;
+    private float budget;
+    private ArrayList<Table> tables;
+    
+    //Menu menu;
+}
