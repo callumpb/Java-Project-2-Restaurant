9273f5806d229045bd5e06163dfe71607bad6b50
Callum <callumpb@hotmail.com>
2017-11-19 21:57:47 +0000

    initial Patron instantiation;

--- app/src/main/java/com/example/user/restaurant/Patron.java
+++ app/src/main/java/com/example/user/restaurant/Patron.java
@@ -1,8 +1,15 @@
 package com.example.user.restaurant;
 
+import java.util.ArrayList;
+
 /**
  * Created by user on 19/11/2017.
  */
 
 public class Patron {
+    private ArrayList<Dish> order;
+
+    public Patron() {
+        this.order = new ArrayList<Dish>();
+    }
 }
