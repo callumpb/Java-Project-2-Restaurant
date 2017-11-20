5aeb482bf5e8fd979cec043db872ed63050ee94e
Callum <callumpb@hotmail.com>
2017-11-20 13:28:58 +0000

    added hasName test, getName method

--- app/src/test/java/com/example/user/restaurant/IngredientTest.java
+++ app/src/test/java/com/example/user/restaurant/IngredientTest.java
@@ -1,9 +1,24 @@
 package com.example.user.restaurant;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by user on 20/11/2017.
  */
 
 public class IngredientTest {
     Ingredient ingredient;
+
+    @Before
+    public void before() {
+        this.ingredient = new Ingredient("Salad");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Salad", ingredient.getName());
+    }
 }
